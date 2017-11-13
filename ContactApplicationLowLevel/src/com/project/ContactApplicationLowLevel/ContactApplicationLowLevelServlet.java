56af146ffdc080bf241f82fd7e140a86dd5db280
irudiproject2017 <irudiproject2017@gmail.com>
2017-11-13 19:14:12 +0530

    Initial Commit

--- ContactApplicationLowLevel/src/com/project/ContactApplicationLowLevel/ContactApplicationLowLevelServlet.java
+++ ContactApplicationLowLevel/src/com/project/ContactApplicationLowLevel/ContactApplicationLowLevelServlet.java
@@ -1,10 +1,12 @@
-package com.practise.ContactApplicationLowLevel;
+package com.project.ContactApplicationLowLevel;
+
 import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class ContactApplicationLowLevelServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
 	}
